b8631fbbee5c63ce06e59fdcf407a8ce66ae2be7
mmorales4050 <mmorales4050@gmail.com>
2019-03-04 10:27:43 -0500

    first file added

---
+++ game.rb
@@ -0,0 +1,45 @@
+
+@top_wall = "---------------------"
+
+@side_wall = "|                   |\n"
+
+@space = "                     "
+
+def make_room
+
+  puts @top_wall
+  puts @side_wall * 10
+  puts @top_wall
+end
+
+make_room
+
+user_input = gets.chomp
+
+puts "\e[H\e[2J"
+
+make_room
+
+user_input = gets.chomp
+
+puts "\e[H\e[2J"
+
+make_room
+
+user_input = gets.chomp
+
+puts "\e[H\e[2J"
+
+make_room
+
+user_input = gets.chomp
+
+puts "\e[H\e[2J"
+
+make_room
+
+user_input = gets.chomp
+
+puts "\e[H\e[2J"
+
+make_room
