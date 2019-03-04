b24a8a295062309531ad1542d6cd60017edc5366
mmorales4050 <mmorales4050@gmail.com>
2019-03-04 14:04:08 -0500

    added player and movement

--- config/enviroment.rb
+++
@@ -1,5 +0,0 @@
-require "bundler"
-Bundler.require 
-ActiveRecord::Base.establish_connection(adaptor: "sqlite3", database: "db/development.db"
-ActiveRecord::Base.logger = nil 
-require_all "lib"
--- lib/room.rb
+++ lib/room.rb
@@ -1,44 +1,124 @@
+require 'io/console'
+
 class Room
 
   def initialize
+    @door_location = nil
+    @player_location = nil
+    @width = 27
+    @height = 12
     @room =
-"--------------------------
-|                        |
-|                        |
-|                        |
-|                        |
-|                        |
-|                        |
-|                        |
-|                        |
-|                        |
---------------------------"
+"---------------------------
+|                         |
+|                         |
+|                         |
+|                         |
+|                         |
+|                         |
+|                         |
+|                         |
+|                         |
+|                         |
+---------------------------"
   end
   # Prints a room to the screen
   def draw_room
     puts @room
   end
 
-  def move_up(location)
-    location - 10
-    @room[]
+  def add_bottom_door
+    @door_location = (@room.length) - (@width/2)
+    @room[@door_location] = "|"
+    @room[@door_location + 1] = "|"
+  end
 
-  def add_player(number)
-    @room[number] = "@"
-    draw_room
-    sleep(1)
+  def spawn_player
+    @player_location = @door_location - @width
+    @room[@player_location] = "@"
+  end
+
+  def move_up
+    if @room[@player_location - (@width + 1)] == " "
+      @room[@player_location] = " "
+      @player_location -= (@width + 1)
+      @room[@player_location] = "@"
+    end
+  end
+
+  def move_down
+    if @room[@player_location + (@width + 1)] == " "
+      @room[@player_location] = " "
+      @player_location += (@width + 1)
+      @room[@player_location] = "@"
+    end
+  end
+
+  def move_right
+    if @room[@player_location + 1] == " "
+      @room[@player_location] = " "
+      @player_location += 1
+      @room[@player_location] = "@"
+    end
+  end
+
+  def move_left
+    if @room[@player_location - 1] == " "
+      @room[@player_location] = " "
+      @player_location -= 1
+      @room[@player_location] = "@"
+    end
+  end
+
+  def clear_screen
     puts "\e[H\e[2J"
-    @room[number] = " "
+  end
+
+  def movement_loop
+    while true
+      draw_room
+      system("stty raw -echo")
+      user_input = STDIN.getc
+      system("stty -raw echo")
+      case user_input
+      when "d"
+        move_right
+      when "a"
+        move_left
+      when "w"
+        move_up
+      when "s"
+        move_down
+      when "p"
+        break
+      else
+        puts "select valid command"
+      end
+      clear_screen
+    end
   end
 end
 
-new_room = Room.new
-
-new_room.add_player(40)
-new_room.add_player(41)
-new_room.add_player(42)
-new_room.add_player(43)
-new_room.add_player(44)
-new_room.add_player(45)
-new_room.add_player(46)
-new_room.add_player(47)
+# puts 'Do you want to proceed? y/n'
+#
+# loop do
+#   system("stty raw -echo")
+#   c = STDIN.getc
+#   system("stty -raw echo")
+#
+#   case c
+#   when 'y'
+#     puts 'Yes'
+#     break
+#   when 'n'
+#     puts 'No'
+#     break
+#   else
+#     puts 'Please type "y" or "n"'
+#   end
+# end
+#
+room = Room.new
+room.clear_screen
+room.add_bottom_door
+room.spawn_player
+room.movement_loop
